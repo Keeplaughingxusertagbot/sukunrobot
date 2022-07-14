5afef554c0cc2e9a7534f93621f24721298728dd
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-14 14:51:12 +0530

    Update approve.py

--- FallenRobot/modules/approve.py
+++ FallenRobot/modules/approve.py
@@ -199,11 +199,11 @@ def unapproveall_btn(update: Update, context: CallbackContext):
 
 __help__ = """
 Sometimes, you might trust a user not to send unwanted content.
-Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
+Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them. 「 sᴜᴋᴜɴ ☯︎ ʀᴏʙᴏᴛ 」
 
 That's what approvals are for - approve of trustworthy users to allow them to send 
 
-*Admin commands:*
+*Admin commands from 「 sᴜᴋᴜɴ ☯︎ ʀᴏʙᴏᴛ 」:*
 ❍ /approval*:* Check a user's approval status in this chat.
 ❍ /approve*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
 ❍ /unapprove*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
