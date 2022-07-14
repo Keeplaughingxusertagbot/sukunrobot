7dc9d4dc4f49963a358b1fb9ce0be84ce9ba852a
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-14 11:54:54 +0530

    Update ping.py

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -80,7 +80,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! 🖤\n"
+        "「 sᴜᴋᴜɴ ☯︎ ʀᴏʙᴏᴛ 」 ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! 🖤\n"
         "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
         "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
