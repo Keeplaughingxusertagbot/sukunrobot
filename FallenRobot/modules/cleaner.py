a8c7bac45b8417a3ce1f99efc2337d41f4410b02
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-14 14:11:16 +0530

    Update cleaner.py

--- FallenRobot/modules/cleaner.py
+++ FallenRobot/modules/cleaner.py
@@ -221,7 +221,7 @@ def bluetext_ignore_list(update: Update, context: CallbackContext):
 
 
 __help__ = """
-*Blue text cleaner* removed any made up commands that people send in your chat.
+*Blue text cleaner* removed any made up commands that people send in your chat from 「 sᴜᴋᴜɴ ☯︎ ʀᴏʙᴏᴛ 」
  ❍ /cleanblue <on/off/yes/no>*:* clean commands after sending
  ❍ /ignoreblue <word>*:* prevent auto cleaning of the command
  ❍ /unignoreblue <word>*:* remove prevent auto cleaning of the command
