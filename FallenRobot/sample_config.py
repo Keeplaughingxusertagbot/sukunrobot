5e667cad7ff756a5133ed580cf0fe943feb89f4a
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-14 07:34:55 +0530

    Update sample_config.py

--- FallenRobot/sample_config.py
+++ FallenRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/FallenRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/sukunxrobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1356469075  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "anonymous_was_bot"
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your own group for support, do not add the @
+    OWNER_ID = 1417375784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Bhumi_ha_r"
+    SUPPORT_CHAT = "sukoon_kii_talash"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001686672798
     )  # Prints any new group the bot is added to, prints just the name and ID.
