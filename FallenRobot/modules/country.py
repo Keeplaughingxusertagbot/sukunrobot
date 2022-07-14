3346f9b86dc76bd49eb90f420cab2879b6c36ebb
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-14 14:00:58 +0530

    Update country.py

--- FallenRobot/modules/country.py
+++ FallenRobot/modules/country.py
@@ -101,7 +101,7 @@ Time Zones:- {tom}
 Top Level Domain:- {lanester}
 wikipedia:- {wiki}</b>
 
-Gathered By [FALLEN](t.me/anonymous_was_bot)</b>
+Gathered By [Sukun](t.me/Bhumi_ha_r)</b>
 """
 
     await borg.send_message(
@@ -112,7 +112,7 @@ Gathered By [FALLEN](t.me/anonymous_was_bot)</b>
 
 
 __help__ = """
-I will give information about a country
+「 sᴜᴋᴜɴ ☯︎ ʀᴏʙᴏᴛ 」 will give information about a country 
 
  ❍ /country <country name>*:* Gathering info about given country
 """
