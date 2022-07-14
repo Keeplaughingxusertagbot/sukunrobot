238b122f1482163002230ecaede22427fe06f444
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-14 14:30:17 +0530

    Update blacklist_stickers.py

--- FallenRobot/modules/blacklist_stickers.py
+++ FallenRobot/modules/blacklist_stickers.py
@@ -497,7 +497,7 @@ def __stats__():
 __help__ = """
 Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
 *NOTE:* Blacklist stickers do not affect the group admin
- ❍ /blsticker*:* See current blacklisted sticker
+ ❍ /blsticker*:* See current blacklisted sticker from 「 sᴜᴋᴜɴ ☯︎ ʀᴏʙᴏᴛ 」
 *Only admin:*
  ❍ /addblsticker <sticker link>*:* Add the sticker trigger to the black list. Can be added via reply sticker
  ❍ /unblsticker <sticker link>*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
